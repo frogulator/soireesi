579992ee55b9e6b5ff6be91c794c29a943a55bce
frogulator <138589139+frogulator@users.noreply.github.com>
2024-01-10 14:09:00 +0100

    initital commit

---
+++ script.js
@@ -0,0 +1,32 @@
+gsap.to("#invitation span", {
+    duration: 1,
+    opacity: 1,
+    y: 0,
+    ease: "back.out(1.7)",
+    stagger: 0.2,
+    repeat: 0,
+    onComplete: startContainerAnimation
+});
+
+function startContainerAnimation() {
+    let tl = gsap.timeline();
+
+    tl
+
+      .fromTo(".container", 
+        { opacity: 0, y: 50 }, 
+        { duration: 1, opacity: 1, y: 0, ease: "back.out(1.7)" } 
+      )
+      .fromTo(".hat", 
+        { opacity: 0, y: 70 }, 
+        { duration: 1, opacity: 1, y: 0, ease: "back.out(1.7)" }, 
+        "-=0.7" 
+      );
+}
+
+
+
+
+
+
+
