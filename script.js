58399d7d2d68ce016259c80b663ae70ba49a87ff
frogulator <138589139+frogulator@users.noreply.github.com>
2024-01-13 16:02:34 +0100

    animation and spacing

--- script.js
+++ script.js
@@ -1,38 +1,33 @@
 gsap.to("#invitation span", {
-    duration: 2,
-    opacity: 1,
-    y: 0,
-    ease: "back.out(1.7)",
-    stagger: 0.2,
-    repeat: 0,
-    onComplete: startContainerAnimation
+  duration: 2,
+  opacity: 1,
+  y: 0,
+  ease: "back.out(1.7)",
+  stagger: 0.2,
+  repeat: 0,
+  onComplete: startContainerAnimation
 });
 
 function startContainerAnimation() {
-    let tl = gsap.timeline();
+  let tl = gsap.timeline();
 
-    tl
+  tl
+    .fromTo(".container", 
+      { opacity: 0, y: 50 }, 
+      { duration: 1, opacity: 1, y: 0, ease: "back.out(1.7)" }
+    )
+    .fromTo(".stars", 
+      { opacity: 0, y: 70 }, 
+      { duration: 1, opacity: 1, y: 0, ease: "back.out(1.7)" }, 
+      "-=0.7" 
+    );
 
-      .fromTo(".container", 
-        { opacity: 0, y: 50 }, 
-        { duration: 1, opacity: 1, y: 0, ease: "back.out(1.7)" } 
-      )
-      .fromTo(".stars", 
-        { opacity: 0, y: 70 }, 
-        { duration: 1, opacity: 1, y: 0, ease: "back.out(1.7)" }, 
-        "-=0.7" 
-      );
+  
+  gsap.to(tl, {duration: 2, delay: 5});
 }
 
-function adjustHeight() {
-  var vh = window.innerHeight * 0.01;
-  var desiredHeight = 94 * vh; 
-  document.querySelector('.part-one').style.height = `${desiredHeight}px`; // Set the height
-}
 
-// Run the function on load and on resize
-window.addEventListener('load', adjustHeight);
-window.addEventListener('resize', adjustHeight);
+
 
 
 
