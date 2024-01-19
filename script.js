5d239f608f312a4eda827d8f2b779a4c1178a254
frogulator <138589139+frogulator@users.noreply.github.com>
2024-01-19 17:51:11 +0100

    delay

--- script.js
+++ script.js
@@ -8,30 +8,7 @@ gsap.to("#invitation span", {
   
 });
 
-gsap.to(".container", {
-  duration: 2,
-  opacity: 1,
-  delay: 2,
-  y: -50,
-  ease: "back.out(1.7)",
-  stagger: 0.2,
-  repeat: 0,
-
-});
-// function startContainerAnimation() {
-//   let tl = gsap.timeline();
-
-//   tl
-//     .fromTo(".container", 
-//       { opacity: 0, y: 50 }, 
-//       { duration: 3, opacity: 1, y: 0, ease: "back.out(1.7)" }
-//     )
-//   gsap.to(tl, {duration: 3, delay: 0});
-// }
-
-
-// gsap.fromTo(".container", 
-//       { opacity: 0, y: 50 }, 
-//       { duration: 3, opacity: 1, y: 0, ease: "back.out(1.7)" }
-//     )
-
+gsap.fromTo(".container", 
+      { opacity: 0, y: 100 }, 
+      { duration: 3, opacity: 1, y: 0, ease: "back.out(2)", delay: 1 }
+    )
