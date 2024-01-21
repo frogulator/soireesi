54312e0d30cae7fa594f98ab969d906e9f2ed208
frogulator <138589139+frogulator@users.noreply.github.com>
2024-01-21 21:07:37 +0100

    rsvp

--- rsvp/rsvp.js
+++ rsvp/rsvp.js
@@ -1,28 +1,19 @@
 
-    document.getElementById('rsvpForm').addEventListener('submit', function(event) {
-        event.preventDefault();
+document.getElementById('rsvpForm').addEventListener('submit', function(event) {
+    event.preventDefault();
 
-        var formData = {
-            name: document.getElementById('name').value,
-            email: document.getElementById('email').value,
-            attendance: document.getElementById('attendance').value,
-            guests: document.getElementById('guests').value,
-            comments: document.getElementById('comments').value
-        };
-
-        fetch('http://yourserver.com/sendemail', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify(formData),
-        })
-        .then(response => response.json())
-        .then(data => {
-            console.log('Success:', data);
-        })
-        .catch((error) => {
-            console.error('Error:', error);
-        });
+    emailjs.send("service_e4ojo08", "template_juq56y9", {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        attendance: document.getElementById('attendance').value,
+        guests: document.getElementById('guests').value,
+        comments: document.getElementById('comments').value
+    })
+    .then(function(response) {
+        console.log('SUCCESS!', response.status, response.text);
+    }, function(error) {
+        console.log('FAILED...', error);
     });
+});
+
 
